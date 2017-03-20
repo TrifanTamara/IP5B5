44f3236eb8de86da36b3286bd0605a9df3de32c1
adipalihovici <adipalihovici@users.noreply.github.com>
2017-03-20 11:02:39 +0200

    Barebone

---
+++ Cont.java
@@ -0,0 +1,26 @@
+// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.geocities.com/kpdus/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   Cont.java
+
+import java.util.Vector;
+
+public abstract class Cont
+{
+
+    public Cont()
+    {
+    }
+
+    public String getName()
+    {
+        return null;
+    }
+
+    public void setName()
+    {
+    }
+
+    public int ID;
+    public Vector myManager;
+}
---
+++ Manager.java
@@ -0,0 +1,29 @@
+// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.geocities.com/kpdus/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   Manager.java
+
+import java.util.Vector;
+
+public class Manager
+{
+
+    public Manager()
+    {
+    }
+
+    public Manager getInstance()
+    {
+        return null;
+    }
+
+    public boolean getTicket()
+    {
+        return false;
+    }
+
+    public Manager manager;
+    public Eveniment evenimente;
+    public Cont conturi;
+    public Vector myEveniment;
+}
