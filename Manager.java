9219e8c20d6d9488fe3d37dce8410cea212d8fec
Vasile Catana <vasica38@gmail.com>
2017-04-03 11:26:24 +0300

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -12,14 +12,15 @@ public class Manager {
 
     private static Manager manager=null;
 
-    public Cont[] conturi;
+    private static  ArrayList<Cont> conts;
 
     static public ArrayList<Eveniment>  evenimentDisp;
 
     static public int k =0;
 
     static public Manager getInstance() {
-        evenimentDisp = new ArrayList<Eveniment>(10);
+        evenimentDisp = new ArrayList<Eveniment>(100);
+        conts = new ArrayList<Cont>(100);
        // for (int i=0; i<10; ++i) evenimentDisp.add(
         if(manager==null)
         {
@@ -41,35 +42,7 @@ public class Manager {
 
     }
 
-    public boolean getTicket(Eveniment e, int nr) {
-        if (e.getNrTickets()>nr-1) {
-            e.setAvailable(e.getNrTickets() - nr);
-            return true;
-        } else {
-            return false;
-        }
 
-
-    }
-
-    public  boolean existaCont(Cont x){
-       for(int i=0; i<Cont.length; ++i)
-           if(conturi[i].id==x.id)
-               return true;
-       return false;
-   }
-   public void adaugaCont(Cont x){
-       int ok=0;
-       if (!existaCont(x)){
-           for(int i=0; i<x.name.size(); ++i)
-                if((x.name[i]<'a' or x.name[i]>'z') or (x.name[i]<'A' or x.name[i]>'Z')) then
-                        if(x.name[i]!=' ')
-                            ok=1;
-           if (ok==0) 
-               conturi.add(x);
-       }
-           
-   }
 
 
     public String toString()
@@ -96,6 +69,60 @@ public class Manager {
             }
         }
         return  false;
-
     }
+
+    public boolean existEvent(String event) {
+        for (int i=0; i<evenimentDisp.size(); ++i)
+        {
+            System.out.println(evenimentDisp.get(i).getName());
+            if (evenimentDisp.get(i).getName().equals( event) && evenimentDisp.get(i).getNrTickets()>0 ) {
+                return true;
+            }
+        }
+        return  false;
+    }
+
+    public void addEvent(Eveniment eveniment)
+    {
+        for (int i=0; i<evenimentDisp.size(); ++i)
+        {
+            if (evenimentDisp.get(i).getName()==eveniment.getName())
+            {
+                return;
+            }
+        }
+        evenimentDisp.add(eveniment);
+    }
+
+    public void addUser(Cont user)
+    {
+        if (user.equals("")) return ;
+        for (int i=0; i<conts.size(); ++i)
+        {
+            if (conts.get(i).getName().equals(user.getName()))
+            {
+                return;
+            }
+        }
+        conts.add(user);
+    }
+//
+//    public  boolean existaCont(Cont x){
+//        for(int i=0; i<Cont.size(); ++i)
+//            if(conturi.get(i).id==x.id)
+//                return true;
+//        return false;
+//    }
+//    public void adaugaCont(Cont x){
+//        int ok=0;
+//        if (!existaCont(x)){
+//            for(int i=0; i<x.name.size(); ++i)
+//                if((x.name[i]<'a' or x.name[i]>'z') or (x.name[i]<'A' or x.name[i]>'Z')) then
+//            if(x.name[i]!=' ')
+//                ok=1;
+//            if (ok==0)
+//                conturi.add(x);
+//        }
+//
+//    }
 }
