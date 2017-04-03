d100c6fb399e3380d36ca4e6d971c95e88525c3f
TrifanTamara <trifantm@yahoo.com>
2017-04-03 11:13:42 +0300

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -41,7 +41,24 @@ public class Manager {
 
     }
 
-
+    public  boolean existaCont(Cont x){
+       for(int i=0; i<Cont.length; ++i)
+           if(conturi[i].id==x.id)
+               return true;
+       return false;
+   }
+   public void adaugaCont(Cont x){
+       int ok=0;
+       if (!existaCont(x)){
+           for(int i=0; i<x.name.size(); ++i)
+                if((x.name[i]<'a' or x.name[i]>'z') or (x.name[i]<'A' or x.name[i]>'Z')) then
+                        if(x.name[i]!=' ')
+                            ok=1;
+           if (ok==0) 
+               conturi.add(x);
+       }
+           
+   }
 
 
     public String toString()
