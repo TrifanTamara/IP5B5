a24cb398db4308a918c1abfc0a70d20a309b7e2f
Vasile Catana <vasica38@gmail.com>
2017-04-03 10:51:13 +0300

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
  * Created by Vasile Catana on 3/20/2017.
  */
@@ -12,12 +14,13 @@ public class Manager {
 
     public Cont[] conturi;
 
-    public Eveniment[]  myEveniment;
+    static public ArrayList<Eveniment>  evenimentDisp;
 
     static public int k =0;
 
     static public Manager getInstance() {
-
+        evenimentDisp = new ArrayList<Eveniment>(10);
+       // for (int i=0; i<10; ++i) evenimentDisp.add(
         if(manager==null)
         {
             manager = new Manager();
@@ -28,8 +31,8 @@ public class Manager {
     }
 
     public boolean getTicket(Eveniment e) {
-        if (e.getAvailable() > 0) {
-            e.setAvailable(e.getAvailable() - 1);
+        if (e.getNrTickets()>0) {
+            e.setAvailable(e.getNrTickets() - 1);
             return true;
         } else {
             return false;
@@ -37,9 +40,34 @@ public class Manager {
 
 
     }
+
+
+
+
     public String toString()
     {
         return "Eu sint mamanger";
     }
 
+    public Eveniment buyTicket(String nameEvent) {
+        for (int i=0; i<evenimentDisp.size(); ++i)
+        {
+            if (evenimentDisp.get(i).getAvailable()) {
+                return evenimentDisp.get(i);
+            }
         }
+        return  null;
+    }
+
+
+    public boolean existEvent(Eveniment event) {
+        for (int i=0; i<evenimentDisp.size(); ++i)
+        {
+            if (evenimentDisp.get(i).getName() == event.getName()) {
+                return true;
+            }
+        }
+        return  false;
+
+    }
+}
