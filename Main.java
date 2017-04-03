e1d535cf5e65dc5c35195e93f2eafaece6ca2be9
Vasile Catana <vasica38@gmail.com>
2017-04-03 11:14:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,12 +7,34 @@ public class Main {
         Manager manager=Manager.getInstance();
         Manager manager1=Manager.getInstance();
         System.out.println(manager);
-        User cont1 = new User("Vasile",manager);
-        User cont2 = new User("Vladimir",manager);
-        User cont3 = new User("Cristina",manager);
-        User cont4 = new User("Tamara",manager);
+        User user1 = new User("Vasile",manager);
+        User user2 = new User("Vladimir",manager);
+        User user3 = new User("Cristina",manager);
+        User user4 = new User("Tamara",manager);
+
+        //Cont cont1 = new Cont("Vasile",manager);
+        //Cont cont2 = new Cont("Vladimir",manager);
+        //Cont cont3 = new Cont("Cristina",manager);
+        //Cont cont3 = new Cont("Tamara",manager);
+        //
+        Guest guest1 = new Guest("Vasile",manager);
+        Guest guest2 = new Guest("Vladimir",manager);
+        Guest guest3 = new Guest("Cristina",manager);
+        Guest guest = new Guest("Tamara",manager);
 
 
 
+        manager.addEvent(new Eveniment(10,"Untold"));
+        manager.addEvent(new Eveniment(3,"Festivalul Codarii"));
+        manager.addEvent(new Eveniment(400,"Ziua Iasilui"));
+
+        manager.addUser(guest1);
+        manager.addUser(guest2);
+        manager.addUser(guest3);
+        manager.addUser(user2);
+        manager.addUser(user1);
+        manager.addUser(user3);
+
+        guest.buyTicket("Untold");
     }
 }
